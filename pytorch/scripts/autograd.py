bb37bd44c233927308b70b45dffef5bd9d5b7baa
bilalahhmedd <bilalahhmedd@gmail.com>
2022-05-27 16:30:09 +0500

    modified autograd.py

--- pytorch/scripts/autograd.py
+++ pytorch/scripts/autograd.py
@@ -1,1 +1,8 @@
 # this scripts contains code to practice autograd
+# lets see how it works
+
+# autograd is powerfull differentiation auto engine that powers neural network training
+import torch, torchvision
+model = torchvision.models.resnet18(pretrained=True)
+data = torch.rand(1,3,64,64)
+labels = torch.rand(1,1000)
